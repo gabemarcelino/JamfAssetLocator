4a18e20a5266c3ba1479923ced1d8f345fa24af5
Gabriel Marcelino <109539964+gabemarcelino@users.noreply.github.com>
2025-08-14 13:32:38 -0400

    Initial Commit

---
+++ JamfAssetLocator/JamfAssetLocatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  JamfAssetLocatorApp.swift
+//  JamfAssetLocator
+//
+//  Created by Gabriel Marcelino on 8/14/25.
+//
+
+import SwiftUI
+
+@main
+struct JamfAssetLocatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
